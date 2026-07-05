4a8e0f2c088c801bf9126d15577f2efd9c9f1527
CJ Quines <cj@cjquines.com>
2026-07-05 14:56:55 -0400

    fix: download latest wordlist for browser build

--- src/loadWordlist.ts
+++ src/loadWordlist.ts
@@ -3,7 +3,7 @@ import { expand, unpack } from "./front.js";
 /** A function that returns the packed string via fetch. */
 export async function loadPackedString() {
   const resp = await fetch(
-    "https://cdn.jsdelivr.net/npm/cromulence@0.3.2/dist/wordlist.txt",
+    "https://cdn.jsdelivr.net/npm/cromulence@0.6.0/dist/wordlist.txt",
   );
   return await resp.text();
 }
